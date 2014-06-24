18d9cf344fdff9768295678e5738ccad20993e0c
Ryan Robinson <ryan@ryanrobinson.name>
2014-06-24 18:53:49 -0400

    removed the storyboard and added a nib.

---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StoreSearch
+//
+//  Created by Ryan Robinson on 6/24/14.
+//  Copyright (c) 2014 RyanRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
