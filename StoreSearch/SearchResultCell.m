52766176a4b8d4cecc484587a1699464fcdd4f38
Ryan Robinson <ryan@ryanrobinson.name>
2014-07-20 11:28:32 -0400

    Added thumbnails to the search results with AFNetworking.

--- StoreSearch/SearchResultCell.m
+++ StoreSearch/SearchResultCell.m
@@ -6,7 +6,9 @@
 //  Copyright (c) 2014 RyanRobinson. All rights reserved.
 //
 
+#import <AFNetworking/UIImageView+AFNetworking.h>
 #import "SearchResultCell.h"
+#import "SearchResult.h"
 
 @implementation SearchResultCell
 
@@ -33,6 +35,55 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (void)prepareForReuse
+{
+  [super prepareForReuse];
+  [self.artworkImageView cancelImageRequestOperation];
+  self.nameLabel.text = nil;
+  self.artistNameLabel.text = nil;
+}
+
+- (void)configureForSearchResult:(SearchResult *)searchResult
+{
+  self.nameLabel.text = searchResult.name;
+  
+  NSString *artistName = searchResult.artistName;
+  if (artistName == nil) {
+    artistName = @"Unknown";
+  }
+  
+  NSString *kind = [self kindForDisplay:searchResult.kind];
+  self.artistNameLabel.text = [NSString stringWithFormat:@"%@ (%@)", artistName, kind];
+  
+  [self.artworkImageView setImageWithURL:[NSURL URLWithString:searchResult.artworkURL60] placeholderImage:[UIImage imageNamed:@"Placeholder"]];
+}
+
+- (NSString *)kindForDisplay:(NSString *)kind
+{
+  if ([kind isEqualToString:@"album"]) {
+    return @"Album";
+  } else if ([kind isEqualToString:@"audiobook"]) {
+    return @"Audiobook";
+  } else if ([kind isEqualToString:@"book"]) {
+    return @"Book";
+  } else if ([kind isEqualToString:@"ebook"]) {
+    return @"E-Book";
+  } else if ([kind isEqualToString:@"feature-movie"]) {
+    return @"Movie";
+  } else if ([kind isEqualToString:@"music-video"]) {
+    return @"Music Video";
+  } else if ([kind isEqualToString:@"podcast"]) {
+    return @"Podcast";
+  } else if ([kind isEqualToString:@"software"]) {
+    return @"App";
+  } else if ([kind isEqualToString:@"song"]) {
+    return @"Song";
+  } else if([kind isEqualToString:@"tv-episode"]) {
+    return @"TV Episode";
+  }
+  return kind;
 }
 
 @end
--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -87,51 +87,13 @@ static NSString * const LoadingCellIdentifier = @"LoadingCell";
     SearchResultCell *cell = [tableView dequeueReusableCellWithIdentifier:SearchResultCellIdentifier forIndexPath:indexPath];
     
     SearchResult *result = _searchResults[indexPath.row];
-  
-    cell.nameLabel.text = result.name;
     
-    NSString *artistName = result.artistName;
-    if (artistName == nil) {
-      artistName = @"Unknown";
-    }
-    
-    NSString *kind = [self kindForDisplay:result.kind];
-    if (kind == nil) {
-      kind = @"@???";
-    }
-    
-    cell.artistNameLabel.text = [NSString stringWithFormat:@"%@ (%@)", artistName, kind];
+    [cell configureForSearchResult:result];
     
     return cell;
   } else {
     return[tableView dequeueReusableCellWithIdentifier:NothingFoundCellIdentifier forIndexPath:indexPath];
   }
-}
-
-- (NSString *)kindForDisplay:(NSString *)kind
-{
-  if ([kind isEqualToString:@"album"]) {
-    return @"Album";
-  } else if ([kind isEqualToString:@"audiobook"]) {
-    return @"Audiobook";
-  } else if ([kind isEqualToString:@"book"]) {
-    return @"Book";
-  } else if ([kind isEqualToString:@"ebook"]) {
-    return @"E-Book";
-  } else if ([kind isEqualToString:@"feature-movie"]) {
-    return @"Movie";
-  } else if ([kind isEqualToString:@"music-video"]) {
-    return @"Music Video";
-  } else if ([kind isEqualToString:@"podcast"]) {
-    return @"Podcast";
-  } else if ([kind isEqualToString:@"software"]) {
-    return @"App";
-  } else if ([kind isEqualToString:@"song"]) {
-    return @"Song";
-  } else if([kind isEqualToString:@"tv-episode"]) {
-    return @"TV Episode";
-  }
-  return kind;
 }
 
 #pragma mark - UITableViewDelegate
@@ -163,7 +125,14 @@ static NSString * const LoadingCellIdentifier = @"LoadingCell";
 {
   _searchText = searchText;
   
-  [self performSearch];
+  if ([_searchText length] > 0) {
+    [self performSearch];
+  } else {
+    [_queue cancelAllOperations];
+    _isLoading = NO;
+    _searchResults = nil;
+    [self.tableView reloadData];
+  }
 }
 
 #pragma mark - UISegmentedControl
