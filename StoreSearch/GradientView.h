104f01b3e93b30e05b160f698e5b1d036cf9b606
Ryan Robinson <ryan@ryanrobinson.name>
2014-07-27 20:47:04 -0400

    added a gradient view as the background for the detail view.

---
+++ StoreSearch/GradientView.h
@@ -0,0 +1,13 @@
+//
+//  GradientView.h
+//  StoreSearch
+//
+//  Created by Ryan Robinson on 7/27/14.
+//  Copyright (c) 2014 RyanRobinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GradientView : UIView
+
+@end
